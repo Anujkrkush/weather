da0f77aa6399bdca7ed4a0c21d1528362bb97217
Anujkrkush <anujkrkush@gmail.com>
2025-01-27 23:56:52 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -26,3 +26,40 @@ data.info()
 
 # to drop day columns( because this is not required to us)
 data =data.drop(columns=["day"])
+
+#to know number of missing value
+data.isnull().sum()
+data["windspeed"].unique()
+
+#handaling the missing value
+data["winddirection"]=data["winddirection"].fillna(data["winddirection"]).mode()[0]
+data["windspeed"]=data["windspeed"].fillna(data["windspeed"].median())
+
+data.isnull().sum()
+data["rainfall"].unique()
+
+#converting yes=1 and no=0
+data["rainfall"]=data["rainfall"].map({"yes":1, "no":0})
+data.head()
+
+#exploratory data analysis
+sns.set(style="whitegrid")
+
+data.describe()
+data.columns
+
+plt.figure(figsize=(15, 10))
+for i, column in enumerate (['pressure', 'maxtemp', 'temparature', 'mintemp', 'dewpoint', 'humidity','cloud', 'sunshine', 'windspeed'], 1):
+ plt.subplot(3, 3, i)
+ sns.histplot(data[column], kde=True)
+ plt.title(f"Distribution of {column}")
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(6,4))
+sns.countplot(x="rainfall", data=data)
+plt.title("Distrinbution of rainfall")
+plt.show()
+
+#correlation matrix
