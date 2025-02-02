1f41120facbee3ba3b129cf40313ba9b080731c5
Anujkrkush <anujkrkush@gmail.com>
2025-02-03 00:46:21 +0530

    downsample

--- weather.py
+++ weather.py
@@ -63,3 +63,71 @@ plt.title("Distrinbution of rainfall")
 plt.show()
 
 #correlation matrix
+plt.figure(figsize=(10,8))
+sns.heatmap(data.corr(),annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("correlation heatmap")
+plt.show()
+
+plt.figure(figsize=(15, 10))
+for i, column in enumerate (['pressure', 'maxtemp', 'temparature', 'mintemp', 'dewpoint', 'humidity','cloud', 'sunshine', 'windspeed'], 1):
+ plt.subplot(3, 3, i)
+ sns.boxplot(data[column])
+ plt.title(f"boxplot of {column}")
+
+plt.tight_layout()
+plt.show()
+
+#drop highly correlated column
+data=data.drop(columns=['maxtemp', 'temparature', 'mintemp'], errors='ignore')
+
+data.head()
+
+data["rainfall"].value_counts()
+
+#separate majority and minority class
+df_majority = data[data["rainfall"]==1]
+df_minority = data[data["rainfall"]==0]
+
+print(df_majority.shape)
+print(df_minority.shape)
+
+df_majority_downsampled = resample(df_majority, replace=False, n_samples=len(df_minority), random_state=42)
+
+df_downsampled = pd.concat([df_majority_downsampled, df_minority])
+
+df_majority_downsampled.shape
+
+df_downsampled = pd.concat([df_majority_downsampled, df_minority])
+
+df_downsampled.head()
+
+# shuffle the final dataframe
+df_downsampled = df_downsampled.sample(frac=1, random_state=42).reset_index(drop=True)
+
+df_downsampled.head()
+
+df_downsampled["rainfall"].value_counts()
+
+#split feature and target as x and y
+x= df_downsampled.drop(columns=["rainfall"])
+y= df_downsampled["rainfall"]
+
+#splitting the data into training and test
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+#model building
+rf_model = RandomForestClassifier(random_state=42)
+
+param_grid_rf = {
+    "n_estimators": [50, 100, 200],
+    "max_features": ["sqrt", "log2"],
+    "max_depth": [None, 10, 20, 30],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 4]
+}
+
+# hyperparameter tuning using gridsearchcv
+grid_search_rf = GridSearchCV(estimator=rf_model, param_grid=param_grid_rf, cv=5, n_jobs=-1, verbose=2)
+
+grid_search_rf.fit(x_train, y_train)
+
