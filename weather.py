d6f12eb7e25eca4e45a187426b836b7ca407b1e7
Anujkrkush <anujkrkush@gmail.com>
2025-02-03 00:51:18 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -131,3 +131,44 @@ grid_search_rf = GridSearchCV(estimator=rf_model, param_grid=param_grid_rf, cv=5
 
 grid_search_rf.fit(x_train, y_train)
 
+best_rf_model = grid_search_rf.best_estimator_
+print("best parameters for random forest:", grid_search_rf.best_params_)
+
+#Model Evalution
+
+cv_scores =cross_val_score(best_rf_model, x_train, y_train, cv=5)
+print("cross_validatiion scores:",cv_scores)
+print("mean cross-validation scores", np.mean(cv_scores))
+
+#test set performance
+y_pred = best_rf_model.predict(x_test)
+print('test set accuracy:',accuracy_score(y_test,y_pred))
+print("test set confusion matrix \n", confusion_matrix(y_test, y_pred))
+print("classification report:\n",classification_report(y_test,y_pred))
+
+#prediction on unknown data
+
+x_train.columns
+input_data = (1015.9, 19.9, 95, 81, 0.0, 40.0, 13.7)
+
+input_df = pd.DataFrame([input_data], columns=['pressure', 'dewpoint', 'humidity', 'cloud', 'sunshine','winddirection', 'windspeed'])
+
+prediction=best_rf_model.predict(input_df)
+print("prediton result:","rainfall" if prediction[0]==1 else "No Rainfall")
+
+#save model and features to a pickel file
+model_data ={"model": best_rf_model, "feature_names": x.columns.tolist()}
+with open("rainfall_predition_model.pkl","wb") as file:
+    pickle.dump(model_data, file)
+
+#load the saved modek and files and use it for predition
+# load the trained model and feature names from m pickel file
+with open("rainfall_predition_model.pkl", "rb") as file:
+    model_data = pickle.load(file)
+
+input_data = (1015.9, 19.9, 95, 81, 0.0, 40.0, 13.7)
+
+input_df = pd.DataFrame([input_data], columns=['pressure', 'dewpoint', 'humidity', 'cloud', 'sunshine','winddirection', 'windspeed'])
+
+prediction=best_rf_model.predict(input_df)
+print("prediton result:","rainfall" if prediction[0]==1 else "No Rainfall")
